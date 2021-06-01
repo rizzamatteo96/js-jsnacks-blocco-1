35a18a6cd02313fbd239292b832e9a7f23bc7857
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-06-01 14:05:04 +0200

    add first snack

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Snack 1 -  L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
+
+var firstWord = prompt('Inserisci la prima parola');
+var secondWord = prompt('Inserisci la seconda parola');
+
+if(firstWord.length > secondWord.length){
+    document.getElementById('risultato-parole').innerHTML = 'La parola più corta è: ' + secondWord + 'e la più lunga è: ' + firstWord;
+} else if(firstWord.length < secondWord.length){
+    document.getElementById('risultato-parole').innerHTML = 'La parola più corta è: ' + firstWord + ' e la più lunga è: ' + secondWord;
+} else {
+    document.getElementById('risultato-parole').innerHTML = 'Le parole hanno la stessa lunghezza e sono ' + firstWord + ' e ' + secondWord;
+}
+
+
+
+// Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri   inseriti. Esegui questo programma in due versioni, con il for e con il while.
+// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
